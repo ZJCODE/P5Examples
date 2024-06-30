554e221ca9730f418061ac892f522d5631cf7dd1
Jun Zhang <zhangjun129310@gmail.com>
2024-06-30 22:48:57 +0800

    update

--- index.py
+++ index.py
@@ -191,7 +191,7 @@ with c2:
 if success or init_image:
     if init_image:
         url = init_image
-    note.caption("按键盘R键重绘画面 , 按键盘P键暂停/继续动画 , 按键盘S键保存当前图片 , 点击画面后按键盘G键保存3秒的GIF")
+    note.caption("点击画布后 , 按键盘R键重绘画面 , 按键盘P键暂停/继续动画 , 按键盘S键保存当前图片 , 按键盘G键保存3秒的GIF")
     script = """
     
     let img;
