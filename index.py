88cb69b58bef520b9912431b530d7f046b4c2c09
ZhangJun <zhangjun310@live.com>
2024-07-02 15:54:52 +0800

    update

--- index.py
+++ index.py
@@ -86,7 +86,7 @@ st.markdown(
     )
 
 if len(st.session_state.image_hash_set) == 0:
-    init_image = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg/1024px-Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
+    init_image = "http://res.cloudinary.com/dvex4tfg6/image/upload/v1719906825/ArtPlay_demo.jpg"
 else:
     init_image = None
 
@@ -94,7 +94,7 @@ c1,c2 = st.columns([1,5])
 
 with c1:
     with st.expander("像素参数",expanded=True):
-        pixel_shape = pills("像素形状", ["矩形","圆形","三角形"], key="pills_interactive",index=0)
+        pixel_shape = pills("像素形状", ["矩形","圆形","三角形","胶囊"], key="pills_interactive",index=0)
 
         if pixel_shape == "矩形":
             pixel_step = st.slider("像素间距", 3, 100, 20, 1)
@@ -105,7 +105,7 @@ with c1:
             cc1,cc2 = st.columns(2)
             with cc1:
                 if pixel_shape == '矩形':
-                    pixel_size = st.slider("像素宽度", 1, 100, 5, 1)
+                    pixel_size = st.slider("像素宽度", 1, 100, 8, 1)
                 elif pixel_shape == '胶囊':
                     pixel_size = st.slider("像素宽度", 1, 100, 10, 1)
                 else:
@@ -114,7 +114,7 @@ with c1:
                 if pixel_shape == '胶囊':
                     pixel_size_2 = st.slider("像素长度", 1, 100, 35, 1)
                 else:
-                    pixel_size_2 = st.slider("像素长度", 1, 100, 25, 1)
+                    pixel_size_2 = st.slider("像素长度", 1, 100, 16, 1)
         else:
             pixel_size = st.slider("像素大小", 1, 100, 40, 1)
             pixel_size_2 = 0
@@ -188,8 +188,8 @@ with c2:
                     # https://console.cloudinary.com/pm/c-4e2c8dff8262ebfec0ff3aaea4dccd/developer-dashboard
                     # Upload an image
                     upload_result = cloudinary.uploader.upload(file=buffer,
-                                                               public_id= 'ArtPlay_' + str(int(time.time())) + '_' +uploaded_file.name.split(".")[0])
-                    # print(upload_result)
+                                                               public_id= 'ArtPlay_' + uploaded_file.name.split(".")[0])
+                    print(upload_result)
                     url = upload_result["secure_url"]
                     success = True
                     init_image = None
